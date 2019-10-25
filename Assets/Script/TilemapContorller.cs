607cf3f6c22576b0801bd82eb00b367bfb7df5ca
alex <abdurashitob1999@mail.ru>
2019-10-25 14:52:26 +0300

    машинка + новая сцена

--- Assets/Script/TilemapContorller.cs
+++ Assets/Script/TilemapContorller.cs
@@ -2,27 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.UI;
 
 public class TilemapContorller : MonoBehaviour
 {
-
+    public Text txt;
     public TileBase TileToSet;
     public Tilemap map;
     private Camera mainCamera;
+    public Transform Player;
     private TileBase currentTile;
     public TileBase[] rotationRoads;
     public TileBase[] crossRoads;
     public TileBase[] Roads;
     public TileBase[] decoration;
-    // Start is called before the first frame update
+    private Vector3Int clickCellPositionRight;
+    private Vector3 clicWorldPositionRight;
+    private Vector3 moveTo;
+    private BoundsInt area;
     void Start()
     {
-        //map = GetComponent<Tilemap>();
+        //smap = GetComponent<Tilemap>();
         mainCamera = Camera.main;
-        Debug.Log(rotationRoads.Length);
+        clickCellPositionRight = new Vector3Int( 0,0,0);
+        area = map.cellBounds;
+        
+       
     }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -34,7 +40,36 @@ public class TilemapContorller : MonoBehaviour
             swapTile(clickCellPosition, rotationRoads);
             swapTile(clickCellPosition, crossRoads);
             swapTile(clickCellPosition, Roads);
-
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            clicWorldPositionRight = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            clickCellPositionRight = map.WorldToCell(clicWorldPositionRight);
+            clickCellPositionRight = new Vector3Int(clickCellPositionRight.x, clickCellPositionRight.y, 0);
+        }
+        moveTo = map.CellToWorld(clickCellPositionRight);
+        moveTo = new Vector3(moveTo.x, moveTo.y + 0.25f, moveTo.z);
+        Player.position = Vector3.MoveTowards(Player.position, moveTo, Time.deltaTime);
+        if (Input.GetMouseButtonDown(2))
+        {
+            TileBase[] tileArray = map.GetTilesBlock(area);
+            Debug.Log(area);
+            Debug.Log(tileArray.Length);
+            txt.text += "\n";
+            for (int i = 0; i < tileArray.Length; i++)
+            {
+                if (tileArray[i])
+                {
+                    txt.text += "tl=" + i + "\t" + tileArray[i].name.Remove(0, 10) + "   ";
+                    if (i % 10 == 0)
+                        txt.text += "\n";
+                }
+                else { 
+                txt.text += "tl=" + i + "\t" + "NULL  " ;
+                if (i % 10 == 0)
+                        txt.text += "\n";
+                }
+            }
         }
     }
     private void swapTile(Vector3Int point,TileBase[] arr)
@@ -49,3 +84,4 @@ public class TilemapContorller : MonoBehaviour
         }
     }
 }
+//transform.position = Vector3.MoveTowards(Pos1, Pos2, Time.deltaTime*2);
